12f3aea792578e48556c32d450485ba23e7c10e3
Dave Dribin <dribin@crt.realtors.org>
2006-07-12 19:07:39 +0000

    Add login.py
    git-svn-id: https://code.crt.realtors.org/svn/librets/librets/trunk@854 fe7ac059-47f5-0310-8599-d0339b4e125c

---
+++ project/swig/python/login.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+import librets
+
+try:
+  session = librets.RetsSession("http://demo.crt.realtors.org:6103/rets/login")
+  if (not session.Login("Joe", "Schmoe")):
+      print "Invalid login"
+      sys.exit(1)
+
+  print "Action: ", session.GetAction()
+  version = "1.0"
+  if session.GetDetectedRetsVersion() == librets.RETS_1_5:
+    version = "1.5"
+  print "RETS Version: " + version
+
+  logout = session.Logout();
+  print "Billing info: " + logout.GetBillingInfo()
+  print "Logout message: " + logout.GetLogoutMessage()
+  print "Connect time: " + str(logout.GetConnectTime())
+except librets.RetsException,e:
+  print "Caught: " + e.GetMessage()
