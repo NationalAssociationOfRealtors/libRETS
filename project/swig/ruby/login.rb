0142f6ff69d12c274abdb6e47a59b7155aad5ee9
Dave Dribin <dribin@crt.realtors.org>
2006-07-12 19:07:02 +0000

    Add login.rb
    git-svn-id: https://code.crt.realtors.org/svn/librets/librets/trunk@853 fe7ac059-47f5-0310-8599-d0339b4e125c

---
+++ project/swig/ruby/login.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require 'librets'
+require 'pp'
+
+include Librets
+
+session = RetsSession.new("http://demo.crt.realtors.org:6103/rets/login")
+
+if !session.Login("Joe", "Schmoe")
+  puts "Invalid login"
+  exit 1
+end
+
+puts "Action: " + session.GetAction()
+version = "1.0"
+version = "1.5" if (session.GetDetectedRetsVersion() == RETS_1_5)
+puts "RETS Version: " + version
+
+logout = session.Logout()
+
+puts "Billing info: " + logout.GetBillingInfo
+puts "Logout message: " + logout.GetLogoutMessage
+puts "Connect time: " + logout.GetConnectTime.to_s
