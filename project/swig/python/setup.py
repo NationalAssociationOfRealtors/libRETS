707ba352f3a6aa72596deead502231e18530f92d
Dave Dribin <dribin@crt.realtors.org>
2006-07-26 21:53:37 +0000

    Read version number for setup.py from version.mk.
    git-svn-id: https://code.crt.realtors.org/svn/librets/librets/trunk@876 fe7ac059-47f5-0310-8599-d0339b4e125c

--- project/swig/python/setup.py
+++ project/swig/python/setup.py
@@ -1,17 +1,29 @@
 import distutils
 from distutils.core import setup, Extension
 import commands
+import re
 
 librets_config = "../../../librets-config-inplace"
+
+# Read in version from the makefile
+librets_version = "1.0"
+f = open('../../build/version.mk')
+for line in f.readlines():
+    match = re.match('^\\s*VERSION\\s*=\\s*(\\S+)', line)
+    if match:
+        librets_version = match.group(1)
+        break
+f.close()
 
 librets_cflags = commands.getoutput(librets_config + " --cflags").split()
 librets_libs = commands.getoutput(librets_config + " --libs").split()
 
 setup(name = "librets: A RETS client library",
-  version = "1.1.3",
+  version = librets_version,
   py_modules=['librets'],
   ext_modules = [Extension("_librets", ["librets_wrap.cpp"],
     extra_compile_args = librets_cflags,
     extra_link_args = librets_libs
     )
     ])
+
