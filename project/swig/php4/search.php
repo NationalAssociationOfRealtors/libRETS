9b1afb8e5785027c68e1932c4f10ec585af7f85d
Keith T. Garner <kgarner@realtors.org>
2006-12-18 21:16:43 +0000

    Start of search function.  Blocked because returned objects are not acting as objects.
    git-svn-id: https://code.crt.realtors.org/svn/librets/librets/trunk@925 fe7ac059-47f5-0310-8599-d0339b4e125c

---
+++ project/swig/php4/search.php
@@ -0,0 +1,38 @@
+#!/usr/bin/php -q
+<?
+include_once('librets.php');
+
+$session = new RetsSession("http://demo.crt.realtors.org:6103/rets/login");
+
+if (!$session->Login("Joe", "Schmoe"))
+{
+    print "Invalid Login\n";
+    exit(1);
+}
+
+print "Action: " . $session->GetAction() . "\n";
+$version = "1.0";
+if ($session->GetDetectedRetsVersion() == RETS_1_5)
+{
+    $version = "1.5";
+}
+print "RETS Version: " . $version . "\n";
+
+$request = $session->CreateSearchRequest("Property", "ResidentialProperty",
+                                        "(ListPrice=300000-)");
+$request->SetSelect("ListingID,ListPrice,Beds,City");
+$request->SetLimit(SearchRequest_LIMIT_DEFAULT);
+$request->SetOffset(SearchRequest_OFFSET_NONE);
+$request->SetCountType(SearchRequest_RECORD_COUNT_AND_RESULTS);
+$results = $session->Search($request);
+
+print "Record Count: " . $results->GetCount() . "\n";
+
+
+$logout = $session->Logout();
+
+print "Billing info: " . $logout->GetBillingInfo() . "\n";
+print "Logout message: " . $logout->GetLogoutMessage() . "\n";
+print "Connect time: " . $logout->GetConnectTime() . "\n";
+
+?>
