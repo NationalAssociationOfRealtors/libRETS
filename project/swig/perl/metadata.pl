0ed6c81c298510590ad28ea1125ff30eca7edb1d
Keith T. Garner <kgarner@realtors.org>
2007-03-29 15:56:05 +0000

    Updated to be feature equal with the other language example
    git-svn-id: https://code.crt.realtors.org/svn/librets/librets/trunk@966 fe7ac059-47f5-0310-8599-d0339b4e125c

--- project/swig/perl/metadata.pl
+++ project/swig/perl/metadata.pl
@@ -5,6 +5,66 @@ use strict;
 use librets;
 use Data::Dumper;
 
+# Here we define some helper functions
+sub dumpAllTables
+{
+    my $metadata = shift;
+    my $class = shift;
+
+    my $tables = $metadata->GetAllTables($class);
+    foreach my $table (@$tables)
+    {
+        print "Table name: " . $table->GetSystemName() . " [" .
+            $table->GetStandardName() . "]\n";
+        print "\tType: " . $table->GetDataType() . "\n";
+        print "\tUnique: " . $table->IsUnique() . "\n";
+        print "\tMax Length: " . $table->GetMaximumLength() . "\n";
+    }
+}
+
+sub dumpAllClasses
+{
+    my $metadata = shift;
+    my $resource = shift;
+
+    my $classes = $metadata->GetAllClasses($resource->GetResourceID());
+    foreach my $class (@$classes)
+    {
+        print "Class name: " . $class->GetClassName() . " [" .
+            $class->GetStandardName() . "]\n";
+        dumpAllTables($metadata, $class);
+    }
+}
+
+sub dumpAllLookupTypes
+{
+    my $metadata = shift;
+    my $lookup = shift;
+
+    my $lookupTypes = $metadata->GetAllLookupTypes($lookup);
+    foreach my $lt (@$lookupTypes)
+    {
+        print "Lookup value: " . $lt->GetValue() . " (" .
+            $lt->GetShortValue() . ", " . $lt->GetLongValue() . ")\n";
+    }
+}
+
+sub dumpAllLookups
+{
+    my $metadata = shift;
+    my $resource = shift;
+
+    my $lookups = $metadata->GetAllLookups($resource->GetResourceID());
+    foreach my $lookup (@$lookups)
+    {
+        print "Lookup name: " . $lookup->GetLookupName() . " (" .
+            $lookup->GetVisibleName() . ")\n";
+        dumpAllLookupTypes($metadata, $lookup);
+        print "\n";
+    }
+}
+    
+
 my $session = new librets::RetsSession(
     "http://demo.crt.realtors.org:6103/rets/login");
 
@@ -18,12 +78,17 @@ my $metadata = $session->GetMetadata();
 my $system = $metadata->GetSystem();
 print "System ID: " . $system->GetSystemID() . "\n";
 print "Desription: " . $system->GetSystemDescription() . "\n";
-print "Comment : " . $system->GetComments() . "\n";
+print "Comment : " . $system->GetComments() . "\n\n";
 
 my $resources = $metadata->GetAllResources();
 foreach my $resource (@$resources)
 {
-    print $resource->GetStandardName() . "\n"
+    print "Resource name: " . $resource->GetResourceID() . " [" .
+        $resource->GetStandardName() . "]\n";
+    dumpAllClasses($metadata, $resource);
+    dumpAllLookups($metadata, $resource);
+    print "\n";
+    
 }
 
 $session->Logout();
