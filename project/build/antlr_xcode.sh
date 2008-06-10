2220a69009d1ae9e5ff53670eee649b2097c5c54
Keith T. Garner <kgarner@realtors.org>
2008-06-10 15:15:15 +0000

    Add the changes I made to the sqltodmql compiler to xcode build.
    git-svn-id: https://code.crt.realtors.org/svn/librets/librets/trunk@1160 fe7ac059-47f5-0310-8599-d0339b4e125c

--- project/build/antlr_xcode.sh
+++ project/build/antlr_xcode.sh
@@ -7,6 +7,8 @@ SRCDIR=${SRCROOT}/../librets/src
 GRAMMAR=${SRCDIR}/rets-sql.g
 DMQL_TREE=${SRCDIR}/dmql-tree.g
 GET_OBJECT_TREE=${SRCDIR}/get-object-tree.g
+LOOKUP_TREE=${SRCDIR}/lookup-tree.g
+LOOKUP_COLUMNS_TREE=${SRCDIR}/lookup-columns-tree.g
 TRIGGER=${OUTPUT_DIR}/up-to-date
 
 #if [ ${GRAMMAR} -nt ${TRIGGER} ] ||
@@ -15,6 +17,8 @@ TRIGGER=${OUTPUT_DIR}/up-to-date
     ${ANTLR} -o $OUTPUT_DIR ${GRAMMAR}
     ${ANTLR} -o $OUTPUT_DIR -glib ${GRAMMAR} ${DMQL_TREE}
     ${ANTLR} -o $OUTPUT_DIR -glib ${GRAMMAR} ${GET_OBJECT_TREE}
+    ${ANTLR} -o $OUTPUT_DIR -glib ${GRAMMAR} ${LOOKUP_TREE}
+    ${ANTLR} -o $OUTPUT_DIR -glib ${GRAMMAR} ${LOOKUP_COLUMNS_TREE}
     touch ${OUTPUT_DIR}/*.cpp
     touch ${OUTPUT_DIR}/*.hpp
     touch ${OUTPUT_DIR}/*.txt
