9ffc4ba1598ce8675288b6a3c912a8eb3dd2f0a2
Mark Klein <librets@dis.com>
2015-02-09 20:42:34 -0800

    Add librets-build-centos.sh

---
+++ librets-build-centos.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+sudo yum install --assumeyes antlr-tool autoconf boost-devel expat-devel libcurl-devel gcc-c++ swig python-devel
+
+# This is a sample configuration script that I use on FreeBSD for
+# librets development. It can be customized for your own
+# environment.
+
+./autogen.sh 
+
+CPPFLAGS="-L/usr/local/lib -I/usr/local/include"	\
+ ./configure    --enable-examples               	\
+                --enable-debug                  	\
+                --enable-depends                	\
+		--enable-maintainer-documentation	\
+		--enable-shared-dependencies		
+
