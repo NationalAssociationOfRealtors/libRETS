6910240233cff72a78e13bed90f51b8eae060ed8
Mark Lesswing <mark@lesswing.com>
2015-04-28 14:50:20 -0500

    replaced util.log and utils.puts with console.log

--- project/swig/node.js/metadata.js
+++ project/swig/node.js/metadata.js
@@ -4,15 +4,15 @@ var librets = require("./build/Release/librets.node");
 function dump_system(metadata)
 {
     var system = metadata.GetSystem();
-    util.print(util.format("System ID: %s\n", system.GetSystemID()));
-    util.print(util.format('Description: %s\n', system.GetSystemDescription()));
-    util.print(util.format("Comments: %s\n", system.GetComments()));
+    console.log(util.format("System ID: %s\n", system.GetSystemID()));
+    console.log(util.format('Description: %s\n', system.GetSystemDescription()));
+    console.log(util.format("Comments: %s\n", system.GetComments()));
 }
 
 function dump_all_resources(metadata)
 {
     var resources = metadata.GetAllResources();
-    util.puts("");
+    console.log("");
     for (var i = 0; i < resources.size(); i++)
     {
 	dump_all_classes(metadata, resources.get(i));
@@ -28,12 +28,12 @@ function dump_all_classes(metadata, resource)
     for (var i = 0; i < classes.size(); i++)
     {
 	var aClass = classes.get(i);
-	util.print(util.format("Resource name: %s [%s]\n", resourceName,
+	console.log(util.format("Resource name: %s [%s]\n", resourceName,
 			     resource.GetStandardName()));
-	util.print(util.format("Class name: %s [%s]\n", aClass.GetClassName(),
+	console.log(util.format("Class name: %s [%s]\n", aClass.GetClassName(),
 			     aClass.GetStandardName()));
 	dump_all_tables(metadata, aClass);
-	util.puts("");
+	console.log("");
     }
 }
 
@@ -45,20 +45,20 @@ function dump_all_lookups(metadata, resource)
     for (var i = 0; i < lookups.size(); i++)
     {
 	var lookup = lookups.get(i);
-	util.print(util.format("Resource name: %s [%s]\n", resourceName,
+	console.log(util.format("Resource name: %s [%s]\n", resourceName,
 			       resource.GetStandardName()));
-	util.print(util.format("Lookup name: %s (%s)", lookup.GetLookupName(),
+	console.log(util.format("Lookup name: %s (%s)", lookup.GetLookupName(),
 			       lookup.GetVisibleName()));
 
 	//GetMetadataEntryID doesn't look to be exported by swig
 	// if (!lookup.GetMetadataEntryID().empty())
 	// {
-	//     util.print(util.format(" MetadataEntryID: %s",
+	//     console.log(util.format(" MetadataEntryID: %s",
 	//                lookup.GetMetadataEntryID()));
 	// }
-	util.puts("")
+	console.log("")
 	dump_all_lookup_types(metadata, lookup);
-	util.puts("");
+	console.log("");
     }
 }
 
@@ -68,7 +68,7 @@ function dump_all_lookup_types(metadata, lookup)
     for (var i = 0; i < lookupTypes.size(); i++)
     {
 	lt = lookupTypes.get(i);
-	util.print(util.format("Lookup value: %s (%s, %s)\n", lt.GetValue(),
+	console.log(util.format("Lookup value: %s (%s, %s)\n", lt.GetValue(),
 			       lt.GetShortValue(), lt.GetLongValue()));
     }
 }
@@ -79,38 +79,38 @@ function dump_all_tables(metadata, aClass)
     for (var i = 0; i < tables.size(); i++)
     {
 	var table = tables.get(i);
-	util.print(util.format("Table name: %s [%s] (%d)",
+	console.log(util.format("Table name: %s [%s] (%d)",
 			       table.GetSystemName(), table.GetStandardName(),
 			       table.GetDataType()));
 	//GetMetadataEntryID doesn't look to be exported by swig
 	// if (!table.GetMetadataEntryID().empty())
         // {
-        //     util.print(util.format(" MetadataEntryID: " + table.GetMetadataEntryID());
+        //     console.log(util.format(" MetadataEntryID: " + table.GetMetadataEntryID());
         // }
         if (table.InKeyIndex())
         {
-            util.print(" InKeyIndex");
+            console.log(" InKeyIndex");
         }
-	util.puts("");
+	console.log("");
     }
 }
 
 function dump_all_foreign_keys(metadata)
 {
     var foreign_keys = metadata.GetAllForeignKeys();
-    util.puts("");
+    console.log("");
     for (var i = 0; i < foreign_keys.size(); i++)
     {
 	var fk = foreign_keys.get(i);
-	util.print(util.format("Foreign Key ID: %s\n", fk.GetForeignKeyID()));
-        util.print(util.format("  Parent Resource: %s",
+	console.log(util.format("Foreign Key ID: %s\n", fk.GetForeignKeyID()));
+        console.log(util.format("  Parent Resource: %s",
 			       fk.GetParentResourceID()));
-        util.print(util.format(", Class: %s", fk.GetParentClassID()));
-        util.print(util.format(", Name: %s\n", fk.GetParentSystemName()));
-        util.print(util.format("  Child Resource: %s",
+        console.log(util.format(", Class: %s", fk.GetParentClassID()));
+        console.log(util.format(", Name: %s\n", fk.GetParentSystemName()));
+        console.log(util.format("  Child Resource: %s",
 			       fk.GetChildResourceID()));
-        util.print(util.format(", Class: %s", fk.GetChildClassID()));
-        util.print(util.format(", Name: %s\n", fk.GetChildSystemName()));
+        console.log(util.format(", Class: %s", fk.GetChildClassID()));
+        console.log(util.format(", Name: %s\n", fk.GetChildSystemName()));
     }
 }
 
@@ -118,9 +118,12 @@ try
 {
     var session = new librets.RetsSession("http://www.dis.com:6103/rets/login");
 
+    session.SetHttpLogName("trace.txt");                                         
+    session.SetRetsVersion(librets.RETS_1_8_0);                                  
+
     if (!session.Login("Joe", "Schmoe"))
     {
-	util.puts("Login failed");
+	console.log("Login failed");
 	process.exit(-1);
     }
 
